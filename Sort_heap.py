666b45ec2aa1a43c1ab73c304b5738a5aea3f8e5
VitelD <70368450+VitelD@users.noreply.github.com>
2021-07-30 13:45:22 +0300

    Create Sort_heap.py

---
+++ Sort_heap.py
@@ -0,0 +1,50 @@
+import random
+
+def heapsort(list):
+    n = len(list)
+    buildheap(list)
+    H = []
+    #for i in range(n-1, 0, -1):
+    size = n
+    #i = 0
+    while size >= 1:
+        H.append(list[0])
+        list[0], list[size-1] = list[size-1], list[0]
+        list.pop()
+        size -= 1
+        heapify(list, 0, size)
+    
+    return H
+    
+def buildheap(list):
+    n = len(list)
+    par = (n-2) // 2
+
+    while par >= 0:
+        heapify(list, par, n)
+        par -= 1
+        
+        
+def heapify(list, i, size):
+    left = 2*i+1
+    right = 2*i+2
+    
+    if left < size and list[left] > list[i]:
+        j = left
+    else:
+        j = i
+        
+    if right < size and list[right] >  list[j]:
+        j = right
+    
+    if j != i:
+        list[i], list[j] = list[j], list[i]
+        heapify(list, j, size)
+
+list = []
+for i in range(9):
+    k = random.randint(0, 15)
+    list.append(k)
+print(list)
+H = heapsort(list)
+print(H)
